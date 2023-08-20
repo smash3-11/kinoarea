551bfb6574fe1dcb71974fb8b55ae6c84af03bff
Sokhib <websoon010@gmail.com>
2023-08-20 05:08:14 +0500

    fixed n add foo filtered

--- modules/layout.js
+++ modules/layout.js
@@ -1,89 +1,51 @@
 import { headerApi } from "/modules/http.js";
 
+function reloadHeader() {
+    let container = document.querySelector("header .container")
 
-let bgPoster = document.querySelector('.bg')
-let showingAllPosters = false
-
-fetch('https://api.themoviedb.org/3/genre/movie/list', headerApi() )
-  .then((res) => res.json())
-  .then((apiGenres) => {
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', headerApi())
-      .then((res) => res.json())
-      .then((res) => reloadItem(res.results, apiGenres.genres))
-  })
-
-function reloadItem(arr, apiGenres) {
-  let box = document.querySelector('.box')
-  box.innerHTML = ''
-  bgPoster.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${arr[0].backdrop_path})`
-  
-  const toShow = showingAllPosters ? arr.length : 8
-
-  for (let item of arr.slice(0, toShow)) {
-    let div_cover = document.createElement('div')
-    let div_h = document.createElement('div')
-    let div_item = document.createElement('div')
-    let span_item = document.createElement('span')
-    let btn = document.createElement('div')
-    let btn_p = document.createElement('p')
-    let div_text = document.createElement('div')
-    let p_text = document.createElement('p')
-    let span_text = document.createElement('span')
-
-    div_cover.classList.add('c')
-    div_h.classList.add('h')
-    div_item.classList.add('item')
-    span_item.classList.add('rating')
-    btn.classList.add('info_movie')
-    div_text.classList.add('text')
-    p_text.classList.add('name_movie')
-    span_text.classList.add('genre_item')
-
-    div_item.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${item.poster_path})`
-    span_item.innerHTML = item.vote_average
-    p_text.innerHTML = item.title
-    btn_p.innerHTML = "Movie info"
-
-    let genres = []
-    for (let genre of apiGenres) {
-      if (item.genre_ids.includes(genre.id)) {
-        genres.push(genre.name)
-      }
-    }
-    span_text.innerHTML = genres.join(', ')
-
-    div_item.onmouseover = () => {
-      bgPoster.style.transition = 'background-image 0.5s'
-      setTimeout(() => {
-        bgPoster.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-      }, 300)
-    }
-
-    div_item.onmouseout = () => {
-      bgPoster.style.transition = 'background-image 0.5s'
-      setTimeout(() => {
-        bgPoster.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${arr[0].backdrop_path})`
-      }, 300)
-    }
-
-    btn.onclick = () => {
-      const movieId = item.id
-      location.assign(`/pages/movie_info/?id=${movieId}`)
-    }
-    
-    const showPosters = document.querySelector('.btn_all')
-    showPosters.innerHTML = showingAllPosters ? 'Скрыть' : 'Показать все'
-    showPosters.onclick = () => {
-      // showPosters.style.scale = '0.8'
-      showingAllPosters = !showingAllPosters
-      reloadItem(arr, apiGenres)
-    }
-
-    box.append(div_cover)
-    div_cover.append(div_h, div_text)
-    div_h.append(div_item)
-    div_item.append(span_item, btn)
-    btn.append(btn_p)
-    div_text.append(p_text, span_text)
-  }
+    let logo_div = document.createElement('div')
+    logo_div.classList.add('logo')
+    logo_div.innerHTML = `
+       <div class="main_logo">
+        <img src="/public/cin.svg" alt="">
+        <img src="/public/Kinoarea.svg" alt="">
+      </div>
+      <div class="social">
+        <img src="/public/facebook.svg" alt="facebook">
+        <img src="/public/instagram.svg" alt="instagram">
+        <img src="/public/twitter.svg" alt="twitter">
+        <img src="/public/vkontakte.svg" alt="vkontakte">
+      </div>
+      `
+    let nav = document.createElement('nav')
+    nav.classList.add('menu')
+    nav.innerHTML = `
+      <a href="">Афиша</a>
+      <a href="">Медиа</a>
+      <a href="">Фильмы</a>
+      <a href="">Актеры</a>
+      <a href="">Новости</a>
+      <a href="">Подборки</a>
+      <a href="">Категории</a>
+      `
+    let acc_div = document.createElement('div')
+    acc_div.classList.add('profile')
+    acc_div.innerHTML = `
+        <button class="search-btn"><img src="/public/search2.svg" alt=""></button>
+          <button class="login">Войти</button> 
+         `
+    container.append(logo_div, nav, acc_div)
 }
+reloadHeader()
+
+    // const genreContainer = document.getElementById('genreContainer')
+    // fetch('https://api.themoviedb.org/3/genre/movie/list', headerApi() )
+    // .then((res) => res.json())
+    // .then((data) => {
+    //         const genres = data.genres
+    //         genres.forEach(genre => {
+    //             const spanElement = document.createElement('span')
+    //             spanElement.innerHTML = genre.name
+    //             genreContainer.append(spanElement)
+    //         })
+    //     })
