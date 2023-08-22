7cca83533368b430e591818c7f680088fa8179ae
Sokhib <websoon010@gmail.com>
2023-08-22 23:35:21 +0500

    made trailer

--- modules/ui.js
+++ modules/ui.js
@@ -1,7 +1,7 @@
-import { headerApi } from "/modules/http.js";
+import { headerApi } from "/modules/http.js"
 
 
-const genreContainer = document.getElementById('genreContainer')
+const genreContainer = document.querySelector('#genreContainer')
 let bgPoster = document.querySelector('.bg')
 let showingAllPosters = false
 let apiGenres = []
@@ -9,59 +9,58 @@ let movies = []
 
 
 function getGenreIdByName(genreName, genres) {
-    const foundGenre = genres.find(genre => genre.name === genreName);
-    return foundGenre ? foundGenre.id : null;
+  const foundGenre = genres.find(genre => genre.name === genreName)
+  return foundGenre ? foundGenre.id : null
 }
 fetch('https://api.themoviedb.org/3/genre/movie/list', headerApi())
-.then(res => res.json())
-.then(apiGenresData => {
-    apiGenres = apiGenresData.genres;
+  .then(res => res.json())
+  .then(apiGenresData => {
+    apiGenres = apiGenresData.genres
 
-    const genreContainer = document.getElementById('genreContainer');
     apiGenres.forEach(genre => {
-        const spanElement = document.createElement('span');
-        spanElement.innerHTML = genre.name
-        genreContainer.append(spanElement)
+      const spanElement = document.createElement('span')
+      spanElement.innerHTML = genre.name
+      genreContainer.append(spanElement)
 
-        spanElement.onclick = () => {
-          
-          const selectedGenre = spanElement.textContent;
-          const filteredMovies = movies.filter(movie => {
-              return movie.genre_ids.includes(getGenreIdByName(selectedGenre, apiGenres));
-          });
-          reloadItem(filteredMovies, apiGenres);
-          genreContainer.querySelectorAll('span').forEach(span => {
-            if (span === spanElement) {
-                span.classList.add('active_genre');
-            } else {
-                span.classList.remove('active_genre');
-            }
-        });
+      spanElement.onclick = () => {
+
+        const selectedGenre = spanElement.innerHTML
+        const filteredMovies = movies.filter(movie => {
+          return movie.genre_ids.includes(getGenreIdByName(selectedGenre, apiGenres))
+        })
+        reloadItem(filteredMovies, apiGenres)
+        genreContainer.querySelectorAll('span').forEach(span => {
+          if (span === spanElement) {
+            span.classList.add('active_genre')
+          } else {
+            span.classList.remove('active_genre')
+          }
+        })
       }
-    });
+    })
 
     const showAllmovies = document.querySelector('span')
 
     showAllmovies.onclick = () => {
       reloadItem(movies, apiGenres)
       genreContainer.querySelectorAll('span').forEach(span => {
-        span.classList.remove('active_genre');
-    });
-    showAllmovies.classList.add('active_genre');
-  }
+        span.classList.remove('active_genre')
+      })
+      showAllmovies.classList.add('active_genre')
+    }
 
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=2', headerApi())
-        .then(res => res.json())
-        .then(res => {
-            movies = res.results;
-            reloadItem(movies, apiGenres);
-        });
-});
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', headerApi())
+      .then(res => res.json())
+      .then(res => {
+        movies = res.results
+        reloadItem(movies, apiGenres)
+      })
+  })
 function reloadItem(arr, apiGenres) {
   let box = document.querySelector('.box')
   box.innerHTML = ''
   bgPoster.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${arr[0].backdrop_path})`
-  
+
   const toShow = showingAllPosters ? arr.length : 8
 
   for (let item of arr.slice(0, toShow)) {
@@ -117,7 +116,7 @@ function reloadItem(arr, apiGenres) {
       const movieId = item.id
       location.assign(`/pages/movie_info/?id=${movieId}`)
     }
-    
+
     const showPosters = document.querySelector('.btn_all')
     showPosters.innerHTML = showingAllPosters ? 'Скрыть' : 'Показать все'
     showPosters.onclick = () => {
@@ -135,4 +134,171 @@ function reloadItem(arr, apiGenres) {
     div_text.append(p_text, span_text)
   }
 }
+
+
+let iframe = document.querySelector('.item_video iframe')
+let titleTr = document.querySelector('.name_trailer')
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', headerApi())
+  .then(res => res.json())
+  .then(res => {
+    reloadTrailer(res.results)
+
+    if (res.results.length > 0) {
+      fetchTrailer(res.results[0].id)
+    }
+  })
+
+function fetchTrailer(movieId) {
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, headerApi())
+    .then(res => res.json())
+    .then(res => {
+      if (res.results.length > 0) {
+        let videoKey = res.results[0].key
+        iframe.src = `https://www.youtube.com/embed/${videoKey}`
+      }
+    })
+}
+
+function reloadTrailer(arr) {
+  let list_trailer = document.querySelector('.list_trailer_movie')
+  titleTr.innerHTML = arr[0].title
+  for (const item of arr) {
+    let itemMovie = document.createElement('div')
+    itemMovie.classList.add('item_movie')
+    itemMovie.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+
+    let trianglePlay = document.createElement('div')
+    trianglePlay.classList.add('triangle_play')
+
+    let kDiv = document.createElement('div')
+    kDiv.classList.add('k')
+
+    let itemTitle = document.createElement('p')
+    itemTitle.classList.add('item_movie_title')
+    itemTitle.innerHTML = item.title
+
+    let elTrailer = document.createElement('div')
+    elTrailer.classList.add('el_trailer')
+
+    itemMovie.onclick = () => {
+      titleTr.innerHTML = item.title
+      fetchTrailer(item.id) 
+    }
+
+    elTrailer.append(itemMovie)
+    itemMovie.append(trianglePlay, kDiv)
+    elTrailer.append(itemTitle)
+    list_trailer.append(elTrailer)
+  }
+}
+
+
+fetch('https://api.themoviedb.org/3/genre/movie/list', headerApi())
+  .then(res => res.json())
+  .then(apiGenresData => {
+    apiGenres = apiGenresData.genres
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=2', headerApi())
+      .then(res => res.json())
+      .then(res => {
+        movies = res.results
+        reloadPopFilms(movies, apiGenres)
+      })
+  })
+
+function reloadPopFilms(arr, apiGenres) {
+  let popFilm = document.querySelector('.popular_box')
+  popFilm.innerHTML = ''
+
+  for (let item of arr) {
+
+
+
+    let filmContainer = document.createElement('div')
+    filmContainer.classList.add('c')
+
+    let itemContainer = document.createElement('div')
+    itemContainer.classList.add('item')
+    itemContainer.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${item.poster_path})`
+
+
+    let ratingSpan = document.createElement('span')
+    ratingSpan.classList.add('rating')
+    ratingSpan.innerHTML = item.vote_average
+    itemContainer.append(ratingSpan)
+
+    let kDiv = document.createElement('div')
+    kDiv.classList.add('k')
+
+    let infoMovieDiv = document.createElement('div')
+    infoMovieDiv.classList.add('info_movie')
+    infoMovieDiv.innerHTML = '<p>Movie info</p>'
+    kDiv.append(infoMovieDiv)
+    itemContainer.append(kDiv)
+    filmContainer.append(itemContainer)
+
+    let textDiv = document.createElement('div')
+    textDiv.classList.add('text')
+
+    let nameMovieP = document.createElement('p')
+    nameMovieP.classList.add('name_movie')
+    nameMovieP.innerHTML = item.title
+    textDiv.append(nameMovieP)
+
+    let genreSpan = document.createElement('span')
+    genreSpan.classList.add('genre_item')
+    
+    let genres = []
+    for (let genre of apiGenres) {
+      if (item.genre_ids.includes(genre.id)) {
+        genres.push(genre.name)
+      }
+    }
+    genreSpan.innerHTML = genres.join(', ')
+
+    textDiv.append(genreSpan)
+
+    filmContainer.append(textDiv)
+
+    popFilm.append(filmContainer)
+    // wrapper.append(filmContainer);
+  }
+}
+
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+
+
+
+// // // После загрузки данных
+// fetch('https://api.themoviedb.org/3/genre/movie/list', headerApi())
+//   .then(res => res.json())
+//   .then(apiGenresData => {
+//     apiGenres = apiGenresData.genres
+//     fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=2', headerApi())
+//       .then(res => res.json())
+//       .then(res => {
+//         movies = res.results
+//         reloadPopFilms(movies, apiGenres)
+
+//         // Инициализация Swiper
+//         new Swiper('.swiper-container', {
+//           modules: [Navigation, Pagination],
+//           slidesPerView: 4,
+//           spaceBetween: 20,
+//           pagination: {
+//             el: '.swiper-pagination',
+//             type: 'fraction', // Текущий и общий номер слайда
+//           },
+//           navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//           },
+//         });
+//       })
+//   })
 
